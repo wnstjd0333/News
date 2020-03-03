da76450750066c1e792afdc9fa2afd04d022b3f0
kimjunseong <kimjunseong@archive-asia.co.jp>
2020-03-03 18:19:33 +0900

    detailController 수정

--- News/NewsDetailView.swift
+++ News/NewsDetailView.swift
@@ -10,25 +10,25 @@ import UIKit
 
 class NewsDetailView: UIView {
 
-//    @IBOutlet weak var timeLabel: UILabel!
-//    @IBOutlet weak var authorLabel: UILabel!
-//    @IBOutlet weak var headingLabel: UILabel!
-//    @IBOutlet weak var bodyTextView: UITextView!
-//
-//    func configureView(_ model: Article) {
-//
-//        timeLabel.text  = model.publishedAt
-//        headingLabel.text = model.title
-//        bodyTextView.contentOffset = .zero
-//        bodyTextView.text = model.content ?? model.description
-//        authorLabel.text = model.author
-//    }
-//
-//    func clearView() {
-//        timeLabel.text  = ""
-//        headingLabel.text = ""
-//        bodyTextView.text = ""
-//        authorLabel.text = ""
-//    }
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+
+    func configureView(_ model: Article) {
+
+        timeLabel.text  = model.publishedAt
+        headingLabel.text = model.title
+        bodyTextView.contentOffset = .zero
+        bodyTextView.text = model.content ?? model.description
+        authorLabel.text = model.author
+    }
+
+    func clearView() {
+        timeLabel.text  = ""
+        headingLabel.text = ""
+        bodyTextView.text = ""
+        authorLabel.text = ""
+    }
 
 }
